43f72d5c33a1b7144f9f930be08e1216ccc3c199
Julia Villela <juliasvillela@gmail.com>
2023-09-12 11:28:48 +0200

    New functions to measure the average of the temperature.

--- temp.py
+++ temp.py
@@ -2,6 +2,26 @@
 
 TEMP_PATH="/sys/class/thermal/thermal_zone0/"
 
-fo = open(TEMP_PATH + "temp", "r")
-temp = fo.read()
-print(temp)
+#fo = open(TEMP_PATH + "temp", "r")
+#temp = fo.read()
+#print(temp)
+
+def readTemp():
+    fo = open(TEMP_PATH + "temp", "r")
+    value = float(fo.read())
+    temp = value*0.001
+    return temp
+
+times = int(input("How many times do you want to read the temperature? "))
+
+def avg(times):
+    temperature = 0
+
+    for i in range (0,times):
+
+        temperature+=readTemp()
+        
+    average = temperature/times
+    return average
+
+print(avg(times))
